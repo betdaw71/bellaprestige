2d987c308522a8000f48e68c6400ffac82e56577
betdaw71 <babayan.dawid2017@yandex.ru>
2021-05-23 22:55:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.main__corusel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        speed: 500,
+        cssEase: 'linear'
+    });
+});
